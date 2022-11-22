5466f72c392dfbb946580a98b566b9f06a62ef1d
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2022-11-22 19:37:15 +0100

    Ej.2.3 y 2.4: refactored

--- src/js/objetos.js
+++ src/js/objetos.js
@@ -12,28 +12,26 @@ let miEdad = mine.edad;
 console.log(miEdad);
 
 // Una lista que contenga el objeto con tus datos personales y un nuevo objeto con los datos personales de tus dos mejores amig@s
-let bestfriends = {
-  0: {
+
+let list = [
+  { ...mine },
+  {
     nombre: "Alex",
     apellido: "Touchard",
     edad: 35,
     altura: 1.75,
     eresDesarrollador: true,
   },
-  1: {
+  {
     nombre: "Xavi",
     apellido: "Serrano",
     edad: 30,
     altura: 1.7,
     eresDesarrollador: true,
-  }
-}
-
-let list = [mine, bestfriends];
+  },
+];
 console.log(list);
 
 // Una nueva lista con los objetos de la lista anterior ordenados por edad, de mayor a menor
-let it_list = [list[0], {...list[1][0]}, {...list[1][1]}];
-console.log(it_list);
-let ageList = it_list.sort((a, b) => b.edad - a.edad);
+let ageList = list.sort((a, b) => b.edad - a.edad);
 console.log(ageList);
